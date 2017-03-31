c4ef30bf1632dee71449f59c3cc57ab30c522ab0
learn <learn@learn12.local>
2017-03-31 15:33:03 -0700

    add password verification requiring upper and lower cases

--- password_check_challenge.js
+++ password_check_challenge.js
@@ -19,3 +19,49 @@
 // Then output whether the combination the combination is acceptable or not.
 // Run the program and test it for all criteria listed above by inputting different values.
 // Hint: See how the javascript String class can help you.
+
+
+//
+// makes sure both userid and passwords pass the tests
+// verify user id and password functions:
+function isUserIdValid(userID) {
+  return userID.length >= 6 && !userID.includes("#") && !userID.includes("$") && !userID.includes("!")
+}
+function isPasswordValid(passcode) {
+  return passcode.length >= 6  && passcode.toUpperCase() != passcode && passcode.toLowerCase() != passcode && passcode!=("password") && (passcode.includes("#") || passcode.includes("$")  ||  passcode.includes("!"))
+}
+function checkCred(user, password) {
+  return isUserIdValid(user) && isPasswordValid(password);
+}
+//
+function userRegistration() {
+  var username = prompt("Wah yo name?");
+  var pass = prompt("Create yo secret code");
+    if (checkCred(username, pass)===false){
+      alert("Entrance Denied")
+    }
+  else {
+    alert("welcome " + username)}
+}
+
+
+
+
+// if (y.search[/a-z/i] < 1) {
+//     alert("Your password needs a lower case letter")
+// }
+// if (y.search[/A-Z/i] < 1) {
+//     alert("Your password needs an uppser case letter")
+// }
+
+
+ // && passcode!=("password")
+
+
+
+
+
+
+
+
+//
